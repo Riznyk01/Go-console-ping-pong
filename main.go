02e4b3b79b85d3e6c8b5a6d83b81d284671b9e73
Oleksandr Riznyk <OleksandrRiznyk01@gmail.com>
2024-03-15 19:27:09 +0200

    Added movement constraints for rockets

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/eiannone/keyboard"
 	"os"
 	"os/exec"
 	"strings"
@@ -9,12 +10,15 @@ import (
 )
 
 const (
-	cols = 119
-	rows = 27
+	cols       = 120
+	rows       = 27
+	rocketSide = 4
+	filled     = '█'
+	empty      = '░'
+	headerChar = '▬'
+	footerChar = '▬'
+	failPause  = 2 * time.Millisecond
 )
-
-var filled = '█'
-var empty = ' '
 
 type coord struct {
 	x int
@@ -22,23 +26,15 @@ type coord struct {
 }
 
 var scoreA, scoreB = 0, 0
-var delay = 100 * time.Millisecond
-
+var delay = 25 * time.Millisecond
 var ballDirection = &coord{x: 1, y: 1}
-
 var ballCoord = &coord{
-	x: centerCol,
-	y: centerRow,
+	x: centerCol, y: centerRow,
 }
-
 var RocketACoord = &coord{
-	x: 0,
-	y: centerRow,
+	x: 0, y: centerRow,
 }
-
-var RocketSide = 4
-var centerRow = (rows / 2) + 1
-var centerCol = (cols / 2) + 1
+var centerRow, centerCol = (rows / 2) + 1, (cols / 2) + 1
 
 var RocketBCoord = &coord{
 	x: cols,
@@ -46,20 +42,75 @@ var RocketBCoord = &coord{
 }
 
 func main() {
-	// ball movement
-	for {
-		ballCoord.x += ballDirection.x
-		ballCoord.y += ballDirection.y
-		if ballCoord.x == cols || ballCoord.x == 0 {
-			ballDirection.x *= -1
-		}
-		if ballCoord.y == rows || ballCoord.y == 0 {
-			ballDirection.y *= -1
-		}
 
+	go func() {
+		if err := keyboard.Open(); err != nil {
+			panic(err)
+		}
+		defer func() {
+			_ = keyboard.Close()
+		}()
+		for {
+			char, key, err := keyboard.GetKey()
+			if err != nil {
+				panic(err)
+			}
+			if key == keyboard.KeyEsc {
+				break
+			}
+			if char == 'W' || char == 'w' {
+				if RocketACoord.y-rocketSide > 0 {
+					RocketACoord.y -= 1
+				}
+			} else if char == 'S' || char == 's' {
+				if RocketACoord.y+rocketSide < rows {
+					RocketACoord.y += 1
+				}
+			}
+
+			if key == keyboard.KeyArrowUp {
+				if RocketBCoord.y-rocketSide > 0 {
+					RocketBCoord.y -= 1
+				}
+			} else if key == keyboard.KeyArrowDown {
+				if RocketBCoord.y+rocketSide < rows {
+					RocketBCoord.y += 1
+				}
+			}
+		}
+	}()
+
+	for {
+		ballMovement()
 		printScreen(ballCoord)
 	}
+}
 
+func ballMovement() {
+	ballCoord.x += ballDirection.x
+	ballCoord.y += ballDirection.y
+
+	if ballCoord.x == cols {
+		ballDirection.x *= -1
+		if ballCoord.y < (RocketBCoord.y-rocketSide) || ballCoord.y > (RocketBCoord.y+rocketSide) {
+			ballCoord.x = centerCol
+			ballCoord.y = centerRow
+			scoreA++
+		}
+	} else if ballCoord.x == 0 {
+		ballDirection.x *= -1
+		if ballCoord.y < (RocketACoord.y-rocketSide) || ballCoord.y > (RocketACoord.y+rocketSide) {
+			ballCoord.x = centerCol
+			ballCoord.y = centerRow
+			scoreB++
+		}
+	}
+
+	if ballCoord.y == rows {
+		ballDirection.y *= -1
+	} else if ballCoord.y == 0 {
+		ballDirection.y *= -1
+	}
 }
 func printScreen(ball *coord) {
 	clearConsole()
@@ -72,9 +123,9 @@ func printScreen(ball *coord) {
 		for x := 0; x < cols; x++ {
 			if y == ball.y && x == ball.x {
 				r[x] = filled
-			} else if x == 0 && (y >= (RocketACoord.y-RocketSide) && y <= (RocketACoord.y+RocketSide)) {
+			} else if x == 0 && (y >= (RocketACoord.y-rocketSide) && y <= (RocketACoord.y+rocketSide)) {
 				r[x] = filled
-			} else if x == cols-1 && (y >= (RocketBCoord.y-RocketSide) && y <= (RocketBCoord.y+RocketSide)) {
+			} else if x == cols-1 && (y >= (RocketBCoord.y-rocketSide) && y <= (RocketBCoord.y+rocketSide)) {
 				r[x] = filled
 			} else {
 				r[x] = empty
@@ -93,12 +144,13 @@ func clearConsole() {
 	cmd.Run()
 }
 func headerBuilder() string {
-	title := " PING-PONG "
-	filler := strings.Repeat("▒", (cols-len(title))/2)
+	title := " Go PING-PONG "
+	filler := strings.Repeat(string(headerChar), (cols-len(title))/2)
 	return filler + title + filler
 }
 func footerBuilder() string {
-	footerText := fmt.Sprintf("▒ SCORE A:%d ▒ SCORE B:%d ", scoreA, scoreB)
-	filler := strings.Repeat("▒", cols-len(footerText))
-	return footerText + filler
+	a := fmt.Sprintf("%c SCORE A: %v ", footerChar, scoreA)
+	b := fmt.Sprintf(" SCORE B: %v %c", scoreB, footerChar)
+	filler := strings.Repeat("▬", cols-len(a)-len(b))
+	return a + filler + b
 }
